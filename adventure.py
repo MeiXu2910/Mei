643d1a5bb79b71d4c75caf29080825dd28546ae2
MeiXu2910 <168126411+MeiXu2910@users.noreply.github.com>
2024-05-04 23:52:14 -0400

    Add files via upload

--- adventure.py
+++ adventure.py
@@ -2,16 +2,32 @@ import json
 import sys
 
 class Room:
-    def __init__(self, name, desc, exits, items=None):
+    def __init__(self, name, desc, exits, items=None, locked=False):
         self.name = name
         self.desc = desc
         self.exits = exits
         self.items = items if items else []
+        self.locked = locked
+
+    def describe_room(self):
+        print(f'> {self.name}\n\n{self.desc}\n')
+        if self.items:
+            print(f'Items: {", ".join(self.items)}\n')
+        print(f'Exits: {" ".join(self.exits.keys())}\n')
 
 class Player:
     def __init__(self):
         self.current_room = None
         self.inventory = []
+        self.command_history = []
+
+    def add_to_history(self, command):
+        self.command_history.append(command)
+
+    def show_history(self):
+        print("Command History:")
+        for command in self.command_history:
+            print(command)
 
     def describe_room(self):
         print(f'> {self.current_room.name}\n\n{self.current_room.desc}\n')
@@ -68,6 +84,22 @@ def load_map(map_file):
         map_data = json.load(f)
     return map_data
 
+def lock_room(self, room_name):
+        if room_name in self.current_room.exits.values():
+            room = [r for r in self.current_room.exits.values() if r.name == room_name][0]
+            room.locked = True
+            print(f"The {room_name} is now locked.")
+        else:
+            print(f"There's no exit to {room_name}.")
+
+def unlock_room(self, room_name):
+    if room_name in self.current_room.exits.values():
+        room = [r for r in self.current_room.exits.values() if r.name == room_name][0]
+        room.locked = False
+        print(f"The {room_name} is now unlocked.")
+    else:
+        print(f"There's no exit to {room_name}.")
+
 def main():
     map_data = load_map(sys.argv[1])
     rooms = {}
@@ -90,20 +122,19 @@ def main():
             if not action:
                 continue
             verb = action[0]
+            player.add_to_history(" ".join(action))
             if verb == "go":
                 if len(action) < 2:
                     print("Sorry, you need to 'go' somewhere.")
                     continue
                 direction = action[1]
                 if direction in player.current_room.exits:
-                    if player.go(direction):
-                        player.look()
+                    next_room = player.current_room.exits[direction]
+                    if next_room.locked:
+                        print(f"The {next_room.name} is locked. You can't go there.")
                     else:
-                        close_matches = [di for di in player.current_room.exits if direction in di]
-                        if close_matches:
-                            print(f"Did you want to go {close_matches[0]} or {close_matches[1]}?")
-                        else:
-                            print(f"There's no way to go {direction}.")
+                        if player.go(direction):
+                            player.look()
                 else:
                     print(f"There's no way to go {direction}.")
             elif verb == "look":
@@ -127,6 +158,20 @@ def main():
                     continue
                 item = action[1]
                 player.drop(item)
+            elif verb == "history":  # 新添加的 history 命令
+                player.show_history()
+            elif verb == "lock":  # 新添加的 lock 命令
+                if len(action) < 2:
+                    print("Sorry, you need to specify a room to lock.")
+                    continue
+                room_name = " ".join(action[1:])
+                player.lock_room(room_name)
+            elif verb == "unlock":  # 新添加的 unlock 命令
+                if len(action) < 2:
+                    print("Sorry, you need to specify a room to unlock.")
+                    continue
+                room_name = " ".join(action[1:])
+                player.unlock_room(room_name)
             else:
                 print("I don't understand that command.")
         except EOFError:
